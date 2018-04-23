1d5a55e04382a15adf026a5fd9e91cd10e046c17
Nick Tallant <ndtallant@uchicago.edu>
2018-04-23 01:05:39 -0500

    Ready to publish

---
+++ 4_Putting_It_Together/helper.py
@@ -0,0 +1,19 @@
+# YOUR NAME HERE 
+# TA SESSION 4 Intro To Programming
+# helper.py
+
+def get_ranks(input_dict):
+    '''
+    Say what the funtion does here... this may be a 
+    great place for hw instructions as a reminder...
+    Comments describing the function outside of the
+    doc string will result in points off on the hw!
+
+    Input:  input_dict - dictionary {'str': [list]}
+    Output: ranks - dictionary {'str': {'str': int}}
+
+    Example:
+        Input:  {'X': ['B', 'A', 'C']}
+        Output: {'X': {'A': 2, 'C': 3, 'B': 1}}
+    '''
+    # YOUR CODE HERE
